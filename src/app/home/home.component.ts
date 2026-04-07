d9aede16412d66d2ac725c5eff4fc4f96262c398
Hema Sai Priya <dhsaipriya@gmail.com>
2026-04-07 15:43:31 +0530

    Add files via upload

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit,OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    standalone: false
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  slides = [
+    { img: 'assets/img3.jpeg', 
+   },
+    { 
+      img: 'assets/img4.jpg',
+     heading: 'INTRODUCING GEN AI CHAT BOT',
+     paragraph:'AI Chat Bot provides visitors with instant access to sales or support teams,<br>thereby ensuring real-time communication and instant resolution of queries<br>with AI knowledge.'
+     },
+    { 
+      img: 'assets/img1.jpg',
+      heading: 'INTRODUCING GEN AI CHAT BOT',
+      paragraph:'AI Chat Bot provides visitors with instant access to sales or support teams,<br>thereby ensuring real-time communication and instant resolution of queries<br>with AI knowledge.'
+     }
+  ];
+  currentSlide = 0;
+  slideInterval:any;
+
+  
+
+  ngOnInit(): void {
+    this.startSlideShow();
+   }
+
+   ngOnDestroy(): void {
+     this.stopSlideShow();
+   }
+
+   startSlideShow(){
+    this.slideInterval=setInterval(()=>{
+      this.nextSlide();
+    },6000);
+   }
+
+   stopSlideShow(){
+    if(this.slideInterval){
+      clearInterval(this.slideInterval);
+    }
+   }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+
+
+
+
+  userName: string = '';
+ 
+  constructor(private router: Router) { }
+ 
+  navigateToUpload() {
+    this.router.navigate(['/upload-documents'], { queryParams: { userName: this.userName } });
+  }
+}
+
+
