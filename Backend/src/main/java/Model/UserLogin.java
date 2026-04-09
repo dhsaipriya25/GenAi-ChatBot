08032adae0b1dfc124f7bb6ecd56775f1f381d00
Hema Sai Priya <dhsaipriya@gmail.com>
2026-04-09 11:43:43 +0530

    Add UserLogin class with email and password fields

--- Backend/src/main/java/Model/UserLogin.java
+++ Backend/src/main/java/Model/UserLogin.java
@@ -1,1 +1,33 @@
+package com.bot.loginAndRegisterationApp.model;
+
+//import jakarta.persistence.Entity;
+
+
+public class UserLogin {
+	private String email;
+	private String password;
+
+	public UserLogin(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
 
